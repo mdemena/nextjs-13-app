723b403739aacbbbd930c4e9e8dc16d0b2243177
Marc De Mena <mdemena@gmail.com>
2022-11-27 14:20:17 +0100

    Second Commit

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import styles from './Navigation.module.css'
 
 const links = [{
   label:'Home',
@@ -7,19 +8,23 @@ const links = [{
 {
   label:'About',
   route:'/about'
+},
+{
+    label:'Posts',
+    route:'/posts'
 }]
 
 export function Navigation(){
     return (
-        <header>
+        <header className={styles.header}>
             <nav>
-            <ul>
-                {links.map(({label, route}) => (
-                <li key={route}>
-                    <Link href={route}>{label}</Link>
-                </li>
-                ))}
-            </ul>
+                <ul className={styles.navigation}>
+                    {links.map(({label, route}) => (
+                    <li key={route}>
+                        <Link href={route}>{label}</Link>
+                    </li>
+                    ))}
+                </ul>
             </nav>
         </header>
     )
---
+++ app/posts/[id]/page.jsx
@@ -0,0 +1,7 @@
+export default function Post({params}){
+    const { id } = params
+
+    return (
+        <h1>Est es el post {id}</h1>
+    )
+}
